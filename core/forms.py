b95ae906e6762ec1354bdd06873cce6ab3bd18a0
Vicente <vi.acunam@gmail.com>
2025-12-19 16:31:33 -0300

    Refactorización completa a SaaS Multi-tenant

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 
 class RegistroSaaSForm(forms.Form):
-    # 1. Datos del Dueño
     username = forms.CharField(label="Nombre de Usuario", max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="Correo Electrónico", widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="Contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    
-    # 2. Datos de la Clínica
     nombre_clinica = forms.CharField(label="Nombre de tu Centro/Clínica", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    
